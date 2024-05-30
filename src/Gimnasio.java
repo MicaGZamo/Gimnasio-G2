81ebbd66693ae69ba8ed747b9b879d0dcde138ef
Ing. Ramiro Echenique <ra.echenique@gmail.com>
2024-05-30 11:27:20 -0300

    prueba

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
-/**
- *
- * @author gomez
- */
+
 public class Gimnasio {
 
     /**
@@ -14,6 +7,7 @@ public class Gimnasio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
