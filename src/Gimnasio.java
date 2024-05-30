bb5a3b0a39e99bf92939c50e89977b5fe8c34b21
Micaela.G.Zamo <micazamo1@gmail.com>
2024-05-30 16:28:51 +0200

    p

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -2,9 +2,6 @@
 
 public class Gimnasio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
