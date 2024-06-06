3286f1b4977aff8e6630a1e53c8ce644d5ef82bb
NadiaCcod <163307149+NadiaCcod@users.noreply.github.com>
2024-06-06 09:20:49 -0300

    Todos los metodos ClaseData

--- src/AccesoData/ClaseData.java
+++ src/AccesoData/ClaseData.java
@@ -5,6 +5,7 @@
 package AccesoData;
 
 import Entidades.Clase;
+import Entidades.Entrenador;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.Time;
@@ -12,6 +13,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
@@ -79,13 +81,13 @@ public class ClaseData {
         return clase;
     }
     
-     public Clase buscarClaseHorario(String nombre) {
+     public Clase buscarClasePorHorario(LocalTime horario) {
         Clase clase = null;
-        String sql = "SELECT `id-clase`, `nombre`, `id-entrenador`, `horario`, `capacidad`, `estado-clase` FROM `clases` WHERE nombre=?";
+        String sql = "SELECT `id-clase`, `nombre`, `id-entrenador`, `horario`, `capacidad`, `estado-clase` FROM `clases` WHERE horario=?";
         PreparedStatement ps = null;
         try {
             ps = con.prepareStatement(sql);
-            ps.setString(1, nombre);
+            ps.setTime(1,Time.valueOf( horario) );
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
 
@@ -107,6 +109,45 @@ public class ClaseData {
         return clase;
     }
 
+     
+      public List<Clase>  buscarClasePorEntrenador(String nombre, String apellido) {
+        Clase clase = null;
+        List<Clase> listaClase = new ArrayList<>();
+        Entrenador entrenador = null;
+        String sql = " SELECT clases.*, entrenadores.nombre as nombre_entrenador "
+                      + " FROM `clases`, entrenadores "
+                      + " WHERE clases.`id-entrenador` = entrenadores.`id-entrenador` "
+                      + " AND entrenadores.nombre = ? AND entrenadores.apellido = ? " ;
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, nombre);
+            ps.setString(2, apellido);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                entrenador = new Entrenador();
+                clase = new Clase();
+                clase.setIdClase(rs.getInt("id-clase"));
+                clase.setNombre(rs.getString("nombre"));
+                clase.setIdEntrenador(rs.getInt("id-entrenador"));
+                entrenador.setNombreE(rs.getString("nombre"));
+                clase.setHorario(rs.getTime("horario").toLocalTime());
+                clase.setCapacidad(rs.getInt("capacidad"));
+                clase.setEstado(rs.getBoolean("estado-clase"));
+                System.out.println(rs);
+                listaClase.add(clase);
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Clase");
+            System.out.println("error" + e);
+            e.printStackTrace();
+        }
+        return listaClase;
+    }
+
+     
     public void modificarClase(Clase clase) {
         String sql = "UPDATE `clases` SET nombre=?, `id-entrenador`=?, horario=?, capacidad=?, `estado-clase`=? WHERE `id-clase`=? ";
         PreparedStatement ps = null;
