c858ba20a916a7f3f2b7c0f86555dd40953f3251
Micaela.G.Zamo <micazamo1@gmail.com>
2024-06-07 15:42:58 +0200

    update membresiaData

--- src/AccesoData/MembresiaData.java
+++ src/AccesoData/MembresiaData.java
@@ -15,39 +15,12 @@ public class MembresiaData {
         con = Conexion.getConexion();
     }
 
-    private boolean existeSocio(int idSocio) {
-        String sql = "SELECT COUNT(*) FROM socios WHERE `id-socio` = ?";
-
-        try (PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setInt(1, idSocio);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                return rs.getInt(1) > 0;
-            }
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al verificar la existencia del socio");
-            System.out.println("error " + ex.getMessage());
-            ex.printStackTrace();
-        }
-
-        return false;
-    }
-
     public void guardarMembresia(Membresia membresia) {
-
-        int idSocio = membresia.getSocio().getIdSocio(); // Asegúrate de que tienes un método getIdSocio() en Socio
-
-        if (!existeSocio(idSocio)) {
-            JOptionPane.showMessageDialog(null, "El socio no existe");
-            return;
-        }
-
         String sql = "INSERT INTO `membresias` (`id-socio`, `CantidadPases`, `fecha-inicio`, `fecha-fin`, `costo-membresia`, `estado-m`) VALUES (?, ?, ?, ?, ?, ?)";
 
          try {
             PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, idSocio);
+            ps.setInt(1, membresia.getSocio().getIdSocio());
             ps.setInt(2, membresia.getCantPases());
             ps.setDate(3, Date.valueOf(membresia.getFechaInicio()));
             ps.setDate(4, Date.valueOf(membresia.getFechaFin()));
@@ -57,7 +30,7 @@ public class MembresiaData {
             ResultSet rs = ps.getGeneratedKeys();
 
             if (rs.next()) {
-                membresia.setIdMembresia(rs.getInt(1)); // Asumiendo que el id de la membresía es la primera columna
+                membresia.setIdMembresia(rs.getInt(1)); 
                 JOptionPane.showMessageDialog(null, "Membresía cargada correctamente");
             }
 
--- src/Main.java
+++ src/Main.java
@@ -120,15 +120,18 @@ public class Main {
 // ---------------------------- Socio ----------------------------
 //        Socio socio1 = new Socio("33221100", "Luciano", "Castro", 19, "luchoC@gmail.com", "152534", true);
 ////        Socio socio2 = new Socio("00112233", "Soledad", "Arias", 30, "soledad@gmail.com", "154789", true);
-//        Socio socio3 = new Socio("55667799", "Mariano", "Rojas", 45, "Mariano_boquita@gmail.com", "151478", true);
+ //       Socio socio3 = new Socio("55667799", "Mariano", "Rojas", 45, "Mariano_boquita@gmail.com", "151478", true);
 //        Socio socio4 = new Socio("77889944", "Jazmin", "Torres", 25, "jzmincita99@gmail.com", "152012", true);
-        Socio socio5 = new Socio("55002211", "Jazmin", "Garcia", 29, "jzmi15@gmail.com", "153265", true);
-        SocioData sd = new SocioData();
+    //    Socio socio5 = new Socio("55002211", "Jazmin", "Garcia", 29, "jzmi15@gmail.com", "153265", true);
+       Socio socio7 = new Socio("5896589", "Camilo", "Lorca", 39, "cm8@gmail.com", "153696", true);
+       SocioData sd = new SocioData();
 //        sd.guardarSocio(socio1);
 //        sd.guardarSocio(socio2);
 //        sd.guardarSocio(socio3);
 //        sd.guardarSocio(socio4);
-        sd.guardarSocio(socio5);
+//        sd.guardarSocio(socio5);
+sd.guardarSocio(socio7);
+
 //
 //        List<Socio> listadoSociosNombre = sd.buscarSocioNombre("Jazmin");
 //        System.out.println("--- SOCIO/S POR NOMBRE ---");
@@ -170,7 +173,7 @@ public class Main {
             LocalDate fechaFin = fechaInicio.plusDays(30);
 
             // Crear instancia de Membresia
-            Membresia mem1 = new Membresia(socio5, 20, fechaInicio, fechaFin, 10000, true);
+            Membresia mem1 = new Membresia(socio7, 20, fechaInicio, fechaFin, 10000, true);
 
             // Guardar membresía
             md.guardarMembresia(mem1);
